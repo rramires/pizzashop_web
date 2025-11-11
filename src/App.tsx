2dceda3a7c7cb468ce799c6e45af7d144ef89e72
rramires <ricardo@flexbr.com>
2025-11-11 15:33:28 -0300

    Add and config Helmet.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 // adicione os imports
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 
 import { router } from './routes'
 
 export function App() {
-	// substitua o Hello World
-	return <RouterProvider router={router} />
+	return (
+		<HelmetProvider>
+			<Helmet titleTemplate='%s | PizzaShop' />
+			<RouterProvider router={router} />
+		</HelmetProvider>
+	)
 }
