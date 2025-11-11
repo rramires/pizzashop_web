12f54a9f917027b7e35543b1d8e4f1450923c59c
rramires <ricardo@flexbr.com>
2025-11-11 15:13:10 -0300

    Add and config React Router DOM.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+// adicione os imports
+import { RouterProvider } from 'react-router-dom'
+
+import { router } from './routes'
+
 export function App() {
-	return (
-		<>
-			<h1>Hello World !!!</h1>
-		</>
-	)
+	// substitua o Hello World
+	return <RouterProvider router={router} />
 }
