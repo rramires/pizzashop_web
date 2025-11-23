e6140f8f0439cf3c89cb4f52dc2fca590f6457e2
rramires <ricardo@flexbr.com>
2025-11-23 20:52:05 -0300

    Add App Header.

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,18 +1,14 @@
+import { Header } from '@/components/header'
 import { Outlet } from 'react-router-dom'
 
 export function AppLayout() {
 	return (
-		<>
-			<header>
-				<h1>AppLayout Header</h1>
-			</header>
-			<main>
+		<div className='flex min-h-screen flex-col antialiased'>
+			<Header />
+			<div className='flex flex-1 flex-col gap-4 p-8 pt-6'>
 				{/* Content will change here */}
 				<Outlet />
-			</main>
-			<footer>
-				<p>AppLayout Footer</p>
-			</footer>
-		</>
+			</div>
+		</div>
 	)
 }
--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -4,7 +4,7 @@ import { Pizza } from 'lucide-react'
 export function AuthLayout() {
 	return (
 		<>
-			<div className='grid min-h-screen grid-cols-2'>
+			<div className='grid min-h-screen grid-cols-2 antialiased'>
 				<div className='border-foreground/75 bg-muted text-muted-foreground flex h-full flex-col justify-between border-r p-10'>
 					<div className='text-foreground flex items-center gap-3 text-lg'>
 						<Pizza className='h5 w5' />
