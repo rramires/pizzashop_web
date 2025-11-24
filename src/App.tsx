009134f752e0eac5f6d77aed1a040afd7c3f33c0
rramires <ricardo@flexbr.com>
2025-11-24 15:32:23 -0300

    Add Theme Toggle.

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,16 @@ import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
 
 import { router } from './routes'
+import { ThemeProvider } from './components/theme/theme-provider'
 
 export function App() {
 	return (
 		<HelmetProvider>
-			<Helmet titleTemplate='%s | PizzaShop' />
-			<Toaster richColors closeButton />
-			<RouterProvider router={router} />
+			<ThemeProvider storageKey='pizzashop-theme' defaultTheme='dark'>
+				<Helmet titleTemplate='%s | PizzaShop' />
+				<Toaster richColors closeButton />
+				<RouterProvider router={router} />
+			</ThemeProvider>
 		</HelmetProvider>
 	)
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,7 @@
 import { Separator } from '@radix-ui/react-separator'
 import { Home, Pizza, UtensilsCrossed } from 'lucide-react'
 import { NavLink } from './nav-link'
+import { ThemeToggle } from './theme/theme-toggle'
 
 export function Header() {
 	return (
@@ -18,6 +19,9 @@ export function Header() {
 						Pedidos
 					</NavLink>
 				</nav>
+				<div className='ml-auto flex items-center gap-2'>
+					<ThemeToggle />
+				</div>
 			</div>
 		</div>
 	)
