2b682a6c08b33551a7bb2fbcb98902ad130bf402
rramires <ricardo@flexbr.com>
2025-12-12 20:20:58 -0300

    Add Error 404 Page.

--- src/pages/e404.tsx
+++ src/pages/e404.tsx
@@ -1,22 +1,18 @@
 import { Helmet } from 'react-helmet-async'
+import { Link } from 'react-router-dom'
 
 export function NotFound() {
 	return (
 		<>
 			<Helmet title='Not Found' />
-			<div className='flex h-screen flex-col items-center justify-center'>
-				<h1 className='text-3xl font-bold'>
-					404 - Página não encontrada
-				</h1>
-				<h3 className='font-bold'>
-					Voltar para a
-					<a
-						className='ml-1 text-blue-600 hover:text-blue-800 hover:underline'
-						href='/'
-					>
-						página inicial
-					</a>
-				</h3>
+			<div className='flex h-screen flex-col items-center justify-center gap-2'>
+				<h1 className='text-4xl font-bold'>Página não encontrada!</h1>
+				<p className='text-accent-foreground'>
+					Voltar para o{' '}
+					<Link to='/' className='text-sky-600 dark:text-sky-400'>
+						Dashboard
+					</Link>
+				</p>
 			</div>
 		</>
 	)
