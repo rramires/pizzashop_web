f467aad2ed6840360af51ab5190e7c1c8fe507ac
rramires <ricardo@flexbr.com>
2025-12-14 22:20:47 -0300

    Add SignIn API call.

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,12 @@
 import { zodResolver } from '@hookform/resolvers/zod'
+import { useMutation } from '@tanstack/react-query'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
+import { signIn } from '@/api/sign-in'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
@@ -16,6 +18,10 @@ const signInForm = z.object({
 type SignInFormType = z.infer<typeof signInForm>
 
 export function SignIn() {
+	const { mutateAsync: authenticate } = useMutation({
+		mutationFn: signIn,
+	})
+
 	const {
 		register,
 		handleSubmit,
@@ -26,10 +32,7 @@ export function SignIn() {
 
 	async function handleSignIn(data: SignInFormType) {
 		try {
-			//throw new Error('404 not found.')
-			// await 2s for test
-			console.log(data)
-			await new Promise((resolve) => setTimeout(resolve, 2000))
+			await authenticate({ email: data.email })
 
 			toast.success('Enviamos um link de autenticação para seu e-mail.', {
 				action: {
