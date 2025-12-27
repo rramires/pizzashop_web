024e2aaf66b22fb9744e9a7f8ee70bef24e3c8ed
rramires <ricardo@flexbr.com>
2025-12-27 17:17:10 -0300

    Add API call to Popular Products Chart.

--- src/pages/app/dashboard/popular-products-chart.tsx
+++ src/pages/app/dashboard/popular-products-chart.tsx
@@ -1,3 +1,4 @@
+import { useQuery } from '@tanstack/react-query'
 import { BarChart } from 'lucide-react'
 import {
 	Cell,
@@ -8,15 +9,11 @@ import {
 } from 'recharts'
 import colors from 'tailwindcss/colors'
 
+import {
+	getPopularProducts,
+	type GetPopularProductsResponse,
+} from '@/api/get-popular-products'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
-
-const data = [
-	{ product: 'Pepperoni', amount: 40 },
-	{ product: 'Mussarela', amount: 60 },
-	{ product: 'Marguerita ', amount: 25 },
-	{ product: '4 Queijos', amount: 35 },
-	{ product: 'Frango c/ Catupiry', amount: 50 },
-]
 
 const COLORS = [
 	colors.sky[500],
@@ -26,47 +23,56 @@ const COLORS = [
 	colors.rose[500],
 ]
 
-const renderColors = data.map((_, index) => {
-	return (
-		<Cell
-			key={`cell-${index}`}
-			fill={COLORS[index]}
-			className='stroke-accent hover:opacity-80'
-		/>
-	)
-})
+function renderColors(data: GetPopularProductsResponse) {
+	return data.map((_, index) => {
+		return (
+			<Cell
+				key={`cell-${index}`}
+				fill={COLORS[index]}
+				className='stroke-accent hover:opacity-80'
+			/>
+		)
+	})
+}
 
-const renderLabels = ({
-	cx,
-	cy,
-	midAngle,
-	innerRadius,
-	outerRadius,
-	value,
-	index,
-}: PieLabelRenderProps) => {
-	const RADIAN = Math.PI / 180
-	const radius = 12 + innerRadius + (outerRadius - innerRadius)
-	const x = cx + radius * Math.cos(-midAngle! * RADIAN)
-	const y = cy + radius * Math.sin(-midAngle! * RADIAN)
+function renderLabels(data: GetPopularProductsResponse) {
+	return ({
+		cx,
+		cy,
+		midAngle,
+		innerRadius,
+		outerRadius,
+		value,
+		index,
+	}: PieLabelRenderProps) => {
+		const RADIAN = Math.PI / 180
+		const radius = 12 + innerRadius + (outerRadius - innerRadius)
+		const x = cx + radius * Math.cos(-midAngle! * RADIAN)
+		const y = cy + radius * Math.sin(-midAngle! * RADIAN)
 
-	return (
-		<text
-			x={x}
-			y={y}
-			className='fill-muted-foreground text-xs'
-			textAnchor={x > cx ? 'start' : 'end'}
-			dominantBaseline='central'
-		>
-			{data[index].product.length > 15
-				? data[index].product.substring(0, 13).concat('...')
-				: data[index].product}{' '}
-			({value})
-		</text>
-	)
+		return (
+			<text
+				x={x}
+				y={y}
+				className='fill-muted-foreground text-xs'
+				textAnchor={x > cx ? 'start' : 'end'}
+				dominantBaseline='central'
+			>
+				{data[index].product.length > 15
+					? data[index].product.substring(0, 13).concat('...')
+					: data[index].product}{' '}
+				({value})
+			</text>
+		)
+	}
 }
 
 export function PopularProductsChart() {
+	const { data: popularProducts } = useQuery({
+		queryFn: getPopularProducts,
+		queryKey: ['metrics', 'popular-products'],
+	})
+
 	return (
 		<Card className='col-span-3'>
 			<CardHeader className='pb-8'>
@@ -78,25 +84,27 @@ export function PopularProductsChart() {
 				</div>
 			</CardHeader>
 			<CardContent>
-				<ResponsiveContainer width='100%' height={240}>
-					<PieChart style={{ fontSize: 12 }}>
-						<Pie
-							data={data}
-							type='linear'
-							dataKey='amount'
-							nameKey='product'
-							strokeWidth={2}
-							cx='50%'
-							cy='50%'
-							outerRadius={86}
-							innerRadius={64}
-							labelLine={false}
-							label={renderLabels}
-						>
-							{renderColors}
-						</Pie>
-					</PieChart>
-				</ResponsiveContainer>
+				{popularProducts && (
+					<ResponsiveContainer width='100%' height={240}>
+						<PieChart style={{ fontSize: 12 }}>
+							<Pie
+								data={popularProducts}
+								type='linear'
+								dataKey='amount'
+								nameKey='product'
+								strokeWidth={2}
+								cx='50%'
+								cy='50%'
+								outerRadius={86}
+								innerRadius={64}
+								labelLine={false}
+								label={renderLabels(popularProducts)}
+							>
+								{renderColors(popularProducts)}
+							</Pie>
+						</PieChart>
+					</ResponsiveContainer>
+				)}
 			</CardContent>
 		</Card>
 	)
