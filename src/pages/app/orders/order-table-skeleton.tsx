20102f5c2cc75c12456370a09b1513683cc70c8e
rramires <ricardo@flexbr.com>
2025-12-29 10:14:22 -0300

    Add Order Skeletons.

---
+++ src/pages/app/orders/order-table-skeleton.tsx
@@ -0,0 +1,40 @@
+import { Search } from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
+import { Skeleton } from '@/components/ui/skeleton'
+import { TableCell, TableRow } from '@/components/ui/table'
+
+export function OrderTableSkeleton() {
+	return Array.from({ length: 10 }).map((_, i) => {
+		return (
+			<TableRow key={i}>
+				<TableCell>
+					<Button disabled variant='outline' size='xs'>
+						<Search className='h-3 w-3' />
+					</Button>
+				</TableCell>
+				<TableCell>
+					<Skeleton className='h-4 w-40' />
+				</TableCell>
+				<TableCell>
+					<Skeleton className='h-4 w-32' />
+				</TableCell>
+				<TableCell>
+					<Skeleton className='h-4 w-24' />
+				</TableCell>
+				<TableCell>
+					<Skeleton className='h-4 w-50' />
+				</TableCell>
+				<TableCell>
+					<Skeleton className='h-4 w-16' />
+				</TableCell>
+				<TableCell>
+					<Skeleton className='h-4 w-23' />
+				</TableCell>
+				<TableCell>
+					<Skeleton className='h-4 w-23' />
+				</TableCell>
+			</TableRow>
+		)
+	})
+}
