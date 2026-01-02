05780d422df973d2d895050b9cfd78be059790e3
rramires <ricardo@flexbr.com>
2026-01-02 17:08:54 -0300

    Add Order Status Test.

--- src/components/order-status.tsx
+++ src/components/order-status.tsx
@@ -21,27 +21,42 @@ export function OrderStatus({ status }: OrderStatusProps) {
 	return (
 		<div className='flex items-center gap-2'>
 			{status === 'pending' && (
-				<span className='h-2 w-2 rounded-full bg-slate-400' />
+				<span
+					data-testid='badge'
+					className='h-2 w-2 rounded-full bg-slate-400'
+				/>
 			)}
 
 			{status === 'canceled' && (
-				<span className='h-2 w-2 rounded-full bg-rose-500' />
+				<span
+					data-testid='badge'
+					className='h-2 w-2 rounded-full bg-rose-500'
+				/>
 			)}
 
 			{/* {['processing', 'delivering'].includes(status) && (
-				<span className='h-2 w-2 rounded-full bg-blue-400' />
+				<span data-testid='badge' className='h-2 w-2 rounded-full bg-blue-400' />
 			)} */}
 
 			{status === 'processing' && (
-				<span className='h-2 w-2 rounded-full bg-blue-400' />
+				<span
+					data-testid='badge'
+					className='h-2 w-2 rounded-full bg-blue-400'
+				/>
 			)}
 
 			{status === 'delivering' && (
-				<span className='h-2 w-2 rounded-full bg-blue-700' />
+				<span
+					data-testid='badge'
+					className='h-2 w-2 rounded-full bg-blue-700'
+				/>
 			)}
 
 			{status === 'delivered' && (
-				<span className='h-2 w-2 rounded-full bg-emerald-500' />
+				<span
+					data-testid='badge'
+					className='h-2 w-2 rounded-full bg-emerald-500'
+				/>
 			)}
 
 			<span className='text-muted-foreground font-medium'>
