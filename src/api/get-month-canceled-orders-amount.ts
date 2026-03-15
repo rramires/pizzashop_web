619f0e533b3e61bc568577b9762e55de7498519d
rramires <ricardo@flexbr.com>
2026-03-14 21:47:51 -0300

    Add MSW and mocks for sign-in and dashboard

--- src/api/get-month-canceled-orders-amount.ts
+++ src/api/get-month-canceled-orders-amount.ts
@@ -1,12 +1,12 @@
 import { api } from '@/lib/axios'
 
-export interface GetMonthCanceledOrdersAmounthResponse {
+export interface GetMonthCanceledOrdersAmountResponse {
 	amount: number
 	diffFromLastMonth: number
 }
 
 export async function getMonthCanceledOrdersAmount() {
-	const response = await api.get<GetMonthCanceledOrdersAmounthResponse>(
+	const response = await api.get<GetMonthCanceledOrdersAmountResponse>(
 		'/metrics/month-canceled-orders-amount',
 	)
 	return response.data
