82e1f1a2e9d20e601c30b85291b4895c403163c6
rramires <ricardo@flexbr.com>
2026-03-15 21:43:48 -0300

    Add Update Profile UI tests.

--- src/api/mocks/update-profile-mock.ts
+++ src/api/mocks/update-profile-mock.ts
@@ -7,7 +7,7 @@ export const updateProfileMock = http.put<never, UpdateProfileBody>(
 	async ({ request }) => {
 		const { name } = await request.json()
 
-		if (name === 'Pizza Shop Test') {
+		if (name === 'Pizza Shop Updated') {
 			return new HttpResponse(null, { status: 204 })
 		}
 
---
+++ test/update-profile.spec.ts
@@ -0,0 +1,31 @@
+import { expect, test } from '@playwright/test'
+
+import { waitForUIInspection } from './utils'
+
+test('update profile successfully', async ({ page }) => {
+	await page.goto('/', { waitUntil: 'networkidle' })
+
+	await page.getByRole('button', { name: 'Pizza Shop' }).click()
+	await page.getByRole('menuitem', { name: 'Perfil da loja' }).click()
+
+	await page.getByLabel('Nome').fill('Pizza Shop Updated')
+	await page.getByLabel('Descrição').fill('Another Description')
+
+	await page.getByRole('button', { name: 'Salvar' }).click()
+
+	await page.waitForLoadState('networkidle')
+
+	const toast = page.getByText('Perfil atualizado com sucesso!')
+
+	expect(toast).toBeVisible()
+
+	await page.getByRole('button', { name: 'Cancelar' }).click()
+
+	await page.waitForTimeout(250)
+
+	expect(
+		page.getByRole('button', { name: 'Pizza Shop Updated' }),
+	).toBeVisible()
+
+	await waitForUIInspection(page)
+})
